e2e7c721884b76a9437cc7709895ba3692ab8d05
Rafael Paes Leme <rafaelspaesleme.ads@gmail.com>
2020-03-15 12:55:07 -0300

    my-blog-version:dev::art: Refatorando codigos de repositorios

--- src/main/java/com/rplproject/myblog/repositories/ArtigoRepository.java
+++ src/main/java/com/rplproject/myblog/repositories/ArtigoRepository.java
@@ -1,9 +1,12 @@
 package com.rplproject.myblog.repositories;
 
 import com.rplproject.myblog.entities.Artigo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArtigoRepository extends JpaRepository<Artigo, Long> {
+    Page<Artigo> findAllByActive(Boolean active, Pageable pageable);
 }
--- src/main/java/com/rplproject/myblog/repositories/PortfolioRepository.java
+++ src/main/java/com/rplproject/myblog/repositories/PortfolioRepository.java
@@ -1,9 +1,12 @@
 package com.rplproject.myblog.repositories;
 
 import com.rplproject.myblog.entities.Portfolio;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
+    Page<Portfolio> findAllByActive(Boolean active, Pageable pageable);
 }
