3c2e44ecf1eaf5e21f766837097342cef5334cb3
Rafael Paes Leme <rafaelspaesleme.ads@gmail.com>
2020-03-15 12:59:10 -0300

    my-blog-version:dev::art: Retirando codigos inutilizados na classe PostagemService

--- src/main/java/com/rplproject/myblog/services/PostagemService.java
+++ src/main/java/com/rplproject/myblog/services/PostagemService.java
@@ -12,7 +12,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 @Service
 public class PostagemService {
