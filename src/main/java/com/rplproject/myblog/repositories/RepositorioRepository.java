eeaae34dae8b1e8e13b7c0c8477aeb5a57d36edc
Rafael Paes Leme <rafaelspaesleme.ads@gmail.com>
2020-03-15 22:57:11 -0300

    my-blog-version:dev::art: Add find by active in class repository of data persistence.

--- src/main/java/com/rplproject/myblog/repositories/RepositorioRepository.java
+++ src/main/java/com/rplproject/myblog/repositories/RepositorioRepository.java
@@ -1,9 +1,12 @@
 package com.rplproject.myblog.repositories;
 
 import com.rplproject.myblog.entities.Repositorio;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RepositorioRepository extends JpaRepository<Repositorio, Long> {
+    Page<Repositorio> findAllByActive(Boolean active, Pageable pageable);
 }
