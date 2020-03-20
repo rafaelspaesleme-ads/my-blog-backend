94a03f5c4f66c54261c11bef2066aa9c97d171ec
Rafael Paes Leme <rafaelspaesleme.ads@gmail.com>
2020-03-19 22:13:16 -0300

    my-blog-version:dev::racehorse: modifying docker-compose.yml

--- execute-docker-compose.sh
+++ execute-docker-compose.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+echo ''
+echo ''
 echo 'Press enter to continue: '
 read enter
+
+mkdir "compose"
+
+mv docker-compose.yml compose
+
+mv compose/docker-compose.yml .
+
+rm -R compose
 
 docker-compose up -d
