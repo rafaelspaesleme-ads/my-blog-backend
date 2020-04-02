e590c5b68cf57fa87bb95c7ddac8f98792b38a73
Rafael Paes Leme <11760799+rafaelspaesleme-ads@users.noreply.github.com>
2020-04-02 17:11:28 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,6 @@
+#!/bin/bash
+
 echo 'Criando tabelas do banco de dados... . . .  .  .  .    .'
 
 sed -e "s|create|none|" -i src/main/resources/application-TYPE_PROFILE_SPRING.properties
 
-
-if [ TYPE_PROFILE_SPRING != "test" ]
-then
-    mvn clean install -Dmaven.test.skip=true
-else
-    mvn clean install
-fi
